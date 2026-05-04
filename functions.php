9a324e651fb0d2460fd33751df8a3a212676a350
NNdiaye22 <167426501+NNdiaye22@users.noreply.github.com>
2026-05-04 16:56:31 +0200

    feat: ajout Meta Pixel (ID 952494574054153)

--- functions.php
+++ functions.php
@@ -17,6 +17,32 @@ require_once ADS_DIR . '/inc/customizer.php';
 require_once ADS_DIR . '/inc/woocommerce.php';
 
 // -------------------------------------------------------
+// Meta Pixel Facebook (ID : 952494574054153)
+// -------------------------------------------------------
+function ads_meta_pixel() {
+    ?>
+    <!-- Meta Pixel Code -->
+    <script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '952494574054153');
+    fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+    src="https://www.facebook.com/tr?id=952494574054153&ev=PageView&noscript=1"
+    /></noscript>
+    <!-- End Meta Pixel Code -->
+    <?php
+}
+add_action( 'wp_head', 'ads_meta_pixel' );
+
+// -------------------------------------------------------
 // Prix "À partir de" pour les produits variables
 // -------------------------------------------------------
 add_filter( 'woocommerce_variable_price_html', function( $price, $product ) {
